9765eac0eb2dd234d19917803364c87dc1fd4906
josiahse <josiah@josiahse.com>
2021-12-22 23:49:16 -0700

    ts heroku deployment

--- colors/settings.py
+++ colors/settings.py
@@ -141,5 +141,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+
 import django_on_heroku
 django_on_heroku.settings(locals())
